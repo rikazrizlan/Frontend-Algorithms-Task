8e2d280f90fc3d934062532876a831239ee5b99b
Rikaz Rizlan <rikzrikaz@gmail.com>
2021-05-17 08:21:23 +0530

    initial commit

---
+++ checkYuGiOh.js
@@ -0,0 +1,30 @@
+function checkYuGiOh(n) {
+    let numbers = [];
+
+    for(let i=1; i<=n ; i++) {
+        numbers.push(i)
+    }
+
+    if(typeof n === "string" && n.indexOf(" ") >= 0) {
+        console.log(`invalid parameter: "${n}"`);
+    } else {
+        for(let i=0; i<numbers.length; i++) {
+            if(numbers[i] % 2 === 0 && numbers[i] % 3 === 0 && numbers[i] % 5 ===0 ) {
+                numbers[i] = "yu-gi-oh";
+            } else if(numbers[i] % 2 === 0 && numbers[i] % 3 === 0) {
+                numbers[i] = "yu-gi";
+            } else if(numbers[i] % 2 === 0 && numbers[i] % 5 === 0) {
+                numbers[i] = "yu-oh";
+            } else if(numbers[i] % 2 === 0) {
+                numbers[i] = "yu";
+            } else if(numbers[i] % 3 === 0) {
+                numbers[i] = "gi";
+            } else if(numbers[i] % 5 === 0) {
+                numbers[i] = "oh";
+            }
+        }
+        console.log(numbers);
+    }
+}
+
+checkYuGiOh("fizzbuzz is meh");
---
+++ convertFahrToCelcius.js
@@ -0,0 +1,12 @@
+function convertFahrToCelcius(F) {
+    if (typeof F !== "string" && typeof F !== "number" && !Array.isArray(F)) {
+        console.log(`${JSON.stringify(F)} is not a valid number but a/an ${typeof F}.`);
+    } else if (Array.isArray(F)) {
+        console.log(`${F} is not a valid number but a/an array.`);
+    } else {
+        let C = (F-32) * 5/9;
+        console.log(C.toFixed(4));
+    }
+}
+
+convertFahrToCelcius({temp:0});
